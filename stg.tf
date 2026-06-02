f8ff4d879a196be3a663c0479a7a04ba99da3cab
parvgupta <gupta.parveen.tech@gmail.com>
2026-06-02 13:39:07 +0530

    stg deleted

--- stg.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_storage_account" "stgaccount223" {
-    name = "stgacc1223"
-    location = "westus"
-    resource_group_name = "rg-dev"
-    account_tier = "Standard"
-    account_replication_type = "LRS"
-}
